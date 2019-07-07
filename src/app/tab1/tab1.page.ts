b2b21afb889a64a239d510ed54510dd05506d100
hatuankiet1 <kiethao112@yahoo.com.vn>
2019-07-07 20:02:48 +0700

    Horse run method

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  @ViewChild("slides") slides: IonSlides;
 
+  constructor(
+    public toastCtrl: ToastController
+  ) { }
+  ionViewDidLoad() {
+
+  }
+  slidesDidLoad(slides: IonSlides) {
+    slides.startAutoplay();
+
+  }
+  nextSlide() {
+    this.slides.slideNext();
+    this.showToast("Slide has been Next");
+  }
+
+  previouSlide() {
+    this.slides.slidePrev();
+    this.showToast("Slide has been Previous");
+
+  }
+
+  async showToast(message: string) {
+    this.toastCtrl.create({
+      message: message,
+      duration: 500,
+      showCloseButton: true,
+      color: 'dark'
+    }).then(toast => {
+      toast.present();
+    })
+  }
 }
